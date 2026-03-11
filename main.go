53d319617968c5f58058b9e9125ac59ec3145661
TheAntiFish <ethancraigcorcoran@gmail.com>
2026-03-11 10:41:22 +1000

    Added methods for tracking and resetting file server hits

--- main.go
+++ main.go
@@ -1,14 +1,26 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"sync/atomic"
+)
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
 
 func main() {
+	apiCfg := &apiConfig{}
+
 	mux := http.NewServeMux()
 
 	strippedFileServer := http.StripPrefix("/app/", http.FileServer(http.Dir("./")))
 
-	mux.Handle("/app/", strippedFileServer)
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(strippedFileServer))
 	mux.HandleFunc("/healthz", ReadinessEndpoint)
+	mux.HandleFunc("/metrics", apiCfg.PrintFileServerHits())
+	mux.HandleFunc("/reset", apiCfg.ResetFileServerHits())
 
 	server := http.Server{
 		Addr:    ":8080",
@@ -22,4 +34,31 @@ func ReadinessEndpoint(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(200)
 	w.Write([]byte("OK"))
+}
+
+func (cfg *apiConfig) PrintFileServerHits() http.HandlerFunc{
+	return func(w http.ResponseWriter, r *http.Request) {
+		hits := cfg.fileserverHits.Load()
+		
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(200)
+		w.Write([]byte(fmt.Sprintf("Hits: %d", hits)))
+	}
+}
+
+func (cfg *apiConfig) ResetFileServerHits() http.HandlerFunc{
+	return func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Store(0)
+
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(200)
+		w.Write([]byte("Hits reset to 0"))
+	}
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
 }
